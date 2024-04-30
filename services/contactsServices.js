cf399ea3e0b015a1a39dd9278694f55940e141ba
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-04-30 16:37:47 +0300

    fixed methods

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -31,6 +31,8 @@ async function removeContact(contactId) {
   const removedContact = contacts[index];
   contacts.splice(index, 1);
 
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
   return removedContact;
 }
 
@@ -44,6 +46,7 @@ async function addContact(name, email, phone) {
   };
 
   contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
   return newContact;
 }
