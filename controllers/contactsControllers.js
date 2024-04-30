554fa6824ee100a969b77f6bf3d5c535d4686aec
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-04-30 17:33:25 +0300

    Revert "Merge pull request #4 from RuslanKabachok/hw02-express"
    This reverts commit 2da7e6c1f5aca9274437d2bcdaf54fdfcc7314c2, reversing
    changes made to aa21fe7eb4055d9f432d8d52fe929382a45ed789.

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,65 +1,11 @@
-import contactsService from '../services/contactsServices.js';
-import {
-  createContactSchema,
-  updateContactSchema,
-} from '../schemas/contactsSchemas.js';
+import contactsService from "../services/contactsServices.js";
 
-export const getAllContacts = async (req, res) => {
-  const contactList = await contactsService.listContacts();
+export const getAllContacts = (req, res) => {};
 
-  res.status(200).json(contactList);
-};
+export const getOneContact = (req, res) => {};
 
-export const getOneContact = async (req, res) => {
-  const contactId = req.params.id;
-  const contact = await contactsService.getContactById(contactId);
+export const deleteContact = (req, res) => {};
 
-  if (contact) {
-    res.status(200).json(contact);
-  } else {
-    res.status(404).json({ message: 'Not found' });
-  }
-};
+export const createContact = (req, res) => {};
 
-export const deleteContact = async (req, res) => {
-  const contactId = req.params.id;
-  const contact = await contactsService.removeContact(contactId);
-
-  if (contact) {
-    res.status(200).json(contact);
-  } else {
-    res.status(404).json({ message: 'Not found' });
-  }
-};
-
-export const createContact = async (req, res) => {
-  const { error } = createContactSchema.validate(req.body);
-  const { name, email, phone } = req.body;
-
-  if (error) {
-    return res.status(400).json({ message: error.message });
-  }
-
-  const newContact = await contactsService.addContact(name, email, phone);
-  return res.status(201).json(newContact);
-};
-
-export const updateContact = async (req, res) => {
-  const { error } = updateContactSchema.validate(req.body);
-
-  if (error) {
-    return res
-      .status(400)
-      .json({ message: 'Body must have at least one field' });
-  }
-
-  const { id } = req.params;
-
-  const updatedContact = await contactsService.updateContact(id, req.body);
-
-  if (!updatedContact) {
-    return res.status(404).json({ message: 'Not found' });
-  }
-
-  return res.status(200).json(updatedContact);
-};
+export const updateContact = (req, res) => {};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,13 +1,9 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().required(),
-  phone: Joi.number().required(),
-});
+
+})
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string(),
-  email: Joi.string(),
-  phone: Joi.string(),
-}).min(1);
+
+})
