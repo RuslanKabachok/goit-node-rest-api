0f50e174a1df83f5a582e9aa11bd6dbd9578fa47
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-05-29 18:32:44 +0300

    initial commit

---
+++ controllers/contactsControllers.js
@@ -0,0 +1,117 @@
+import contactsService from '../services/contactsServices.js';
+import {
+  createContactSchema,
+  updateContactSchema,
+  updateFavoriteSchema,
+} from '../schemas/contactsSchemas.js';
+import Contact from '../models/contact.js';
+
+export const getAllContacts = async (req, res, next) => {
+  try {
+    const contacts = await Contact.find();
+
+    res.send(contacts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getOneContact = async (req, res, next) => {
+  const contactId = req.params.id;
+
+  try {
+    const contact = await Contact.findById(contactId);
+    if (contact === null) {
+      return res.status(404).send('Book not found');
+    }
+    res.send(contact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteContact = async (req, res) => {
+  const contactId = req.params.id;
+
+  try {
+    const result = await Contact.findByIdAndDelete(contactId);
+    if (result === null) {
+      return res.status(404).send('Book not found');
+    }
+    res.send(contactId);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createContact = async (req, res, next) => {
+  const { error } = createContactSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.message });
+  }
+
+  const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    favorite: req.body.favorite,
+  };
+
+  try {
+    await Contact.create(contact);
+    res.status(201).send('Success!');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateContact = async (req, res, next) => {
+  const { error } = updateContactSchema.validate(req.body);
+  const contactId = req.params.id;
+
+  const contact = {
+    name: req.body.name,
+    email: req.body.email,
+    phone: req.body.phone,
+    favorite: req.body.favorite,
+  };
+
+  try {
+    const result = await Contact.findByIdAndUpdate(contactId, contact, {
+      new: true,
+    });
+
+    if (result === null) {
+      return res.status(404).send('Book not found');
+    }
+
+    res.send(result);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateStatusContact = async (req, res, next) => {
+  const { error } = updateFavoriteSchema.validate(req.body);
+
+  if (error) {
+    return res.status(400).json({ message: error.message });
+  }
+
+  const contactId = req.params.id;
+
+  const favorite = { favorite: req.body.favorite };
+
+  try {
+    const result = await Contact.findByIdAndUpdate(contactId, favorite, {
+      new: true,
+    });
+    if (result === null) {
+      return res.status(404).send('Not found');
+    }
+    return res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
+};
