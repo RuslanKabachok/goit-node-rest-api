9e627414828dcd27a02f30e2df26b4f6a3b19e33
RuslanKabachok <ruslan.kabachok@gmail.com>
2024-07-07 11:53:51 +0300

    upd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import initMongoConnection from './src/db/initMongoConnection.js';
-import setupServer from './server.js';
+import setupServer from './src/server.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
